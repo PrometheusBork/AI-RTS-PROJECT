5743ab0e4d4b0eafaf463687c00852e2d70b6ba3
ITPini <marc6571us@gmail.com>
2024-03-13 10:57:41 +0100

    Add GameObject and Tree classes

---
+++ src/game/objects/GameObject.py
@@ -0,0 +1,9 @@
+class GameObject:
+    def __init__(self, position):
+        self.position = position
+
+    def update(self, game_world):
+        pass
+
+    def render(self, renderer):
+        pass
