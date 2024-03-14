2e1c0a2fe73f39675d863b95f54f6867fcb1e2d7
ITPini <marc6571us@gmail.com>
2024-03-14 09:39:39 +0100

    Simplified expressions

--- src/game/tiles/Tile.py
+++ src/game/tiles/Tile.py
@@ -7,7 +7,7 @@ class Tile:
         self.is_walkable = False
 
     def add_game_object(self, game_object):
-        if self.game_object is None:
+        if self.is_empty():
             self.game_object = game_object
         else: 
             print("Game object already exists in this tile")
@@ -16,7 +16,4 @@ class Tile:
         self.game_object = None
 
     def is_empty(self):
-        if self.game_object is None:
-            return True
-        else: 
-            return False
+        return self.game_object is None
