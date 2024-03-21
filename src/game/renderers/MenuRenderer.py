10077e561b470eeafaf063b2e85e4bc769556cf7
ITPini <marc6571us@gmail.com>
2024-03-21 09:55:57 +0100

    Added style.json for GUI styling and updated MenuRenderer to use the new style

--- src/game/renderers/MenuRenderer.py
+++ src/game/renderers/MenuRenderer.py
@@ -7,7 +7,7 @@ from game.gui.components.Button import Button
 class MenuRenderer:
     def __init__(self, screen_size):
         self.screen_size = screen_size
-        self.gui_manager = pygame_gui.UIManager(screen_size)
+        self.gui_manager = pygame_gui.UIManager(screen_size, 'src/game/gui/style/style.json', enable_live_theme_updates=True)
         self.panel_rect = pygame.Rect(0, 0, 300, 300)
         self.panel_rect.center = (self.screen_size[0] / 2, self.screen_size[1] / 2)
         self.panel = pygame_gui.elements.UIPanel(
