b6a22218aede98569e58a6f8f00a286376f6ec3d
ITPini <marc6571us@gmail.com>
2024-03-25 18:39:31 +0100

    Added InteractionManager and handle interactions in MovementManager

---
+++ src/game/managers/InteractionManager.py
@@ -0,0 +1,34 @@
+from game.interfaces.IAttacker import IAttacker
+from game.interfaces.ICollector import ICollector
+from game.interfaces.IAttackable import IAttackable
+from game.interfaces.ICollectable import ICollectable
+
+
+class InteractionManager:
+    def __init__(self, game_world):
+        self.game_world = game_world
+        self.player_manager = game_world.player_manager
+
+    def handle_interaction(self, movable_object, target_position):
+        row, col = target_position
+        target_object = self.game_world.map[row][col].game_object
+
+        if isinstance(movable_object, ICollector) and isinstance(target_object, ICollectable):
+            self.handle_collectable_interaction(movable_object, target_object, target_position)
+        elif isinstance(movable_object, IAttacker) and isinstance(target_object, IAttackable):
+            self.handle_attackable_interaction(movable_object, target_object, target_position)
+
+    def handle_collectable_interaction(self, movable_object, collectable_object, target_position):
+        row, col = target_position
+        resources_collected = movable_object.collect(collectable_object)
+        player = self.player_manager.get_player_by_unit(movable_object)
+        if player:
+            player.add_resources(resources_collected)
+        if collectable_object.is_destroyed():
+            self.game_world.map[row][col].remove_placeable_object()
+
+    def handle_attackable_interaction(self, movable_object, target_object, target_position):
+        row, col = target_position
+        movable_object.attack(target_object)
+        if target_object.is_destroyed():
+            self.game_world.map[row][col].remove_placeable_object()
