00579471a1f77b41a82399b6230966de388c796b
ITPini <marc6571us@gmail.com>
2024-04-12 12:14:58 +0200

    Refactored GameEngine for AI integration

--- src/game/engines/GameEngine.py
+++ src/game/engines/GameEngine.py
@@ -11,27 +11,34 @@ class GameEngine:
         self.game_world = game_world
         self.game_render = game_render
         self.state_manager = state_manager
-        state_manager.register(self.game_render)
+        state_manager.register(game_render)
         self.event_manager = EventManager(game_world, game_render, state_manager)
         self.clock = pygame.time.Clock()
         self.resource_time = 0
-    
-    def run(self):
         self.event_manager.selection_manager.register_selectable_objects()
         self.event_manager.movement_manager.register_movable_objects()
+    
+    def run(self):
         while self.state_manager.state != GameState.QUIT:
+
+            self.next_step(None)
             self.render()
-            self.clock.tick(60)
-            if self.state_manager.state == GameState.RUNNING:
-                if self.check_game_over() is True:
-                    return("break")
-                self.resource_time += RESOURCE_TICK
-                if self.resource_time >= 4:
-                    for player in self.event_manager.players:
-                        player.add_resources(1)
-                    self.resource_time = 0
-            self.event_manager.handle_events()
-        self.game_render.quit()
+
+        self.quit()
+
+    def next_step(self, action):
+        self.event_manager.handle_ai(action)
+        self.event_manager.handle_events()
+        if self.state_manager.state == GameState.RUNNING:
+            if self.check_game_over() is True:
+                return("break")
+            self.resource_time += RESOURCE_TICK
+            if self.resource_time >= 4:
+                for player in self.event_manager.players:
+                    player.add_resources(10)
+                print("Resources:", [player.resources for player in self.event_manager.players])
+                self.resource_time = 0
+        self.clock.tick(60)
 
     def check_game_over(self):
         for player in self.event_manager.players:
@@ -39,6 +46,14 @@ class GameEngine:
                 print(f"Player {player} has lost the game!")
                 return True
         return False
-    
+
+    def reset(self):
+        self.state_manager.set_state(GameState.RUNNING)
+        self.game_render.reset()
+        self.resource_time = 0
+
     def render(self):
         self.game_render.render()
+
+    def quit(self):
+        self.game_render.quit()
