39cc93bf205a69eec1874e8dea93959cd86d2169
ITPini <marc6571us@gmail.com>
2024-04-20 15:30:26 +0200

    Refactored GameEnv class to simplify initialization and step methods

--- src/ai/envs/GameEnv.py
+++ src/ai/envs/GameEnv.py
@@ -1,53 +1,34 @@
-import random
-
 import gym
 
 from game.maps.Map import Map
 
+
 class GameEnv(gym.Env):
-    def __init__(self, game_world, game_render, game_engine, state_manager, grid_size, tile_size, screen_size):
-        self._grid_size = grid_size
-        self._tile_size = tile_size
-        self._screen_size = screen_size
-
-        self.game_world = game_world
-        self.state_manager = state_manager
-        self.render_engine = game_render
+    def __init__(self, game_engine, game_render):
+        super(GameEnv, self).__init__()
         self.game_engine = game_engine
-
-        self.steps_per_action = 30
-        self.current_step = 0
-
-        while True:
-            self.step(5)
-            if self.game_engine.check_game_over() is True:
-                break
+        self.render_engine = game_render
 
     def step(self, action):
-        observation = None
-        reward = 0.0
-        done = False
-        info = {}
-
-        self.current_step += 1
-
-        #if self.current_step % self.steps_per_action == 0:
-        self.game_engine.next_step(random.randint(1, 6))
-        #else:
-        #    self.game_engine.next_step(None)
-
+        self.game_engine.next_step(action)
         self.render()
 
-        return observation, reward, done, info
+        observation = None
+        reward = 0.0
+        terminated = self.game_engine.check_game_over()
+        truncated = False
+        info = {}
+
+        return observation, reward, terminated, truncated, info
 
     def render(self):
         self.game_engine.render()
 
-    def reset(self):
+    def reset(self, seed=None, options=None):
         new_game = Map.select_map("map1")
-        self.game_engine.reset()
-        self.game_engine.game_world = new_game
-        self.render_engine.game_world = new_game
+        self.game_engine.reset(new_game)
+        print(self.game_engine.game_world.player_manager.players == self.game_engine.event_manager.movement_manager.interaction_manager.game_world.player_manager.players)
+        self.render()
 
     def close(self):
         self.game_engine.quit()
