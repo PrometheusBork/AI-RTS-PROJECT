52dd81fd91a99f10b3ec1c55374ccc4804eca9b0
ITPini <marc6571us@gmail.com>
2024-04-23 11:27:51 +0200

    Add tf-keras to requirements.txt and implement DQL agent in src/ai/agents/DQL.py

--- src/main.py
+++ src/main.py
@@ -3,6 +3,8 @@ from game.engines.RenderEngine import RenderEngine
 from game.managers.StateManager import GameStateManager
 from game.maps.Map import Map
 from ai.envs.GameEnv import GameEnv
+from ai.agents.DQL import DQL
+
 
 def main():
     grid_size = (10, 10) # We should find a way to get the Grid Size from the Map file
@@ -22,15 +24,24 @@ def main():
     game_engine = GameEngine(game_world, game_render, state_manager)
 
     # Create Game Environment instance
-    game_env = GameEnv(game_world, game_render, game_engine, state_manager, grid_size, tile_size, screen_size)
+    game_env = GameEnv(game_engine, game_render)
 
-    # Start the game loop
-    if game_engine.run() != "break":
-        pass
-    else:
-        main()
-        
-    
+    players = list(game_world.player_manager.players)
 
-if __name__ == "__main__": 
+    agent1 = DQL(players[0], 1, 2)
+    agent2 = DQL(players[1], 1, 2)
+
+    agents = [agent1, agent2]
+
+    num_episodes = 100
+    for episode in range(num_episodes):
+        state = game_env.reset() if episode != 0 else None
+        terminated = False
+        while not terminated:
+            actions = (agent.act(state) for agent in agents)
+            next_states, rewards, terminated, truncated, info = game_env.step(actions)
+            state = next_states
+
+
+if __name__ == "__main__":
     main()
