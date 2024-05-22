b609170374abf63982d4d845d0a9b9d5bb211842
ITPini <marc6571us@gmail.com>
2024-05-22 11:36:36 +0200

    Refactored Player class to use dictionary for units and implement unit indexing using priority queue

--- src/game/players/Player.py
+++ src/game/players/Player.py
@@ -1,6 +1,11 @@
+import heapq
+
+
 class Player:
     def __init__(self, starting_resources, base):
-        self.units = set()
+        self.units = dict()
+        self.available_indices = []
+        heapq.heappush(self.available_indices, 1)
         self.resources = starting_resources
         self.base = base
 
@@ -8,13 +13,30 @@ class Player:
         return self.base.is_destroyed()
 
     def add_unit(self, unit):
-        self.units.add(unit)
+        if unit not in self.units.values():
+            if self.available_indices:
+                min_index = heapq.heappop(self.available_indices)
+            else:
+                max_index = max(self.units.keys(), default=0)
+                min_index = max_index + 1
+                heapq.heappush(self.available_indices, min_index + 1)
+            self.units[min_index] = unit
 
     def remove_unit(self, unit):
-        self.units.discard(unit)
+        for index, u in self.units.items():
+            if u == unit:
+                del self.units[index]
+                heapq.heappush(self.available_indices, index)
+                break
 
     def add_resources(self, amount):
         self.resources += amount
         
     def get_base_position(self):
         return self.base.row, self.base.col
+
+    def get_unit_index(self, unit):
+        for index, u in self.units.items():
+            if u == unit:
+                return index
+        return 0
