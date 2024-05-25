ec18de3c32878c73f428b85e1126b512d09e2101
ITPini <marc6571us@gmail.com>
2024-05-25 15:58:33 +0200

    Refactored handle_attackable_interaction method to improve readability and count units_destroyed

--- src/game/managers/InteractionManager.py
+++ src/game/managers/InteractionManager.py
@@ -26,13 +26,17 @@ class InteractionManager:
             self.game_world.remove_game_object(target_position)
 
     def handle_attackable_interaction(self, movable_object, target_object, target_position):
-        if self.player_manager.get_player_by_unit(movable_object) != (self.player_manager.get_player_by_unit(target_object) or self.player_manager.get_player_by_base(target_object)):
+        attacker_player = self.player_manager.get_player_by_unit(movable_object)
+        target_player = self.player_manager.get_player_by_unit(target_object) or self.player_manager.get_player_by_base(target_object)
+
+        if attacker_player != target_player:
             movable_object.attack(target_object)
             if target_object.is_destroyed():
                 self.game_world.remove_game_object(target_position)
-                player = self.player_manager.get_player_by_unit(target_object)
-                if player:
-                    player.remove_unit(target_object)
+                if target_player:
+                    target_player.remove_unit(target_object)
+                if attacker_player:
+                    attacker_player.units_destroyed += 1
 
     def reset(self, game_world):
         self.game_world = game_world
